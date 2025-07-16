217925d6b56e1b488f6d8a8fc0b8cdba4d1c8bbb
mschrome <maplexiao@tencent.com>
2025-07-16 16:09:02 +0800

    Update vercel App.vue

--- src/App.vue
+++ src/App.vue
@@ -63,7 +63,7 @@ const clearResults = () => {
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Hello Vue 3 + Vercel" />
+  <HelloWorld msg="Hello Vue 3 + Vercel Deploy" />
   
   <!-- Vercel Blob 测试区域 -->
   <div class="blob-test-container">
