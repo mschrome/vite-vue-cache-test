5b77a9c58454d8755694fce9a0a5858776d7128e
mschrome <maplexiao@tencent.com>
2025-07-17 11:25:42 +0800

    Update CF App.vue

--- src/App.vue
+++ src/App.vue
@@ -235,7 +235,7 @@ const clearUploadResults = () => {
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Hello Vue 3 + Vercel new" />
+  <HelloWorld msg="Hello Vue 3 + CF" />
   
   <!-- Vercel Blob 管理界面 -->
   <div class="blob-manager-container">
