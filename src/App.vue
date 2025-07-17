ad1113cd3deb7502da913d235ed3baf7936335be
MapleShaw <mapleshaw@gmail.com>
2025-07-17 15:13:26 +0800

    Update dev/* 001 App.vue

--- src/App.vue
+++ src/App.vue
@@ -235,7 +235,7 @@ const clearUploadResults = () => {
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Hello Vue 3 + dev/test001" />
+  <HelloWorld msg="Hello Vue 3 + dev/test001 pro" />
   
   <!-- Vercel Blob 管理界面 -->
   <div class="blob-manager-container">
