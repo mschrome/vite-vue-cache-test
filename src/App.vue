af32b3045ef59f2e0358ef3fadac995c415a3127
mschrome <maplexiao@tencent.com>
2025-07-23 19:22:50 +0800

    Update App.vue try failed

--- src/App.vue
+++ src/App.vue
@@ -227,7 +227,7 @@ const clearUploadResults = () => {
 </script>
 
 <template>
-  <div>
+  
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
