b1ccca31a9eb3e270040d7064c8c688228db34d0
maplexiao <maplexiao@tencent.com>
2025-10-11 17:32:03 +0800

    feat(normal): test home fun

---
+++ edge-functions/[id].js
@@ -0,0 +1,14 @@
+export function onRequestGet({request}) {
+  console.log('Hello, Home!');
+  const geo = request.eo.geo;
+  const res = JSON.stringify({
+    geo: geo,
+  });
+
+  return new Response(res, {
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+}
