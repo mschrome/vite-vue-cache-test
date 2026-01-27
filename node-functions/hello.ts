31ce56ecaa8a0c482af846f6bbf23dcdbac909ef
maplexiao <maplexiao@tencent.com>
2026-01-27 16:12:49 +0800

    feat(test): test node kv

--- node-functions/hello.ts
+++ node-functions/hello.ts
@@ -1,5 +1,5 @@
 export const onRequestGet = async (context: unknown): Promise<Response> => {
-  console.log('Hello, World!', MY_kv);
+  console.log('Hello, World!', your_kv);
   return new Response('Hello, World!', {
     status: 200,
     headers: { 'Content-Type': 'text/plain; charset=UTF-8' },
