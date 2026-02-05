8aa962cd58c537373ae08c45ddbc6fc177b6e3a8
maplexiao <maplexiao@tencent.com>
2026-02-05 14:37:35 +0800

    feat(test): test kv in node

--- node-functions/hellokv.ts
+++ node-functions/hellokv.ts
@@ -1,5 +1,5 @@
 export const onRequestGet = async (context: unknown): Promise<Response> => {
-  console.log('Hello, World!', context.env.WEBHOOK_TOKEN);
+  console.log('Hello, World!', your_kv);
   return new Response('Hello, World!', {
     status: 200,
     headers: { 'Content-Type': 'text/plain; charset=UTF-8' },
